bf0f10852a2befd23a1a39059381da3700a71851
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-04-24 14:46:13 +0200

    feat: update backend configurations and Docker setup
    - Add environment and Docker configuration files for development and production setups.
    - Update Dockerfile to use Eclipse Temurin base image and install necessary packages.
    - Modify docker-compose to integrate environment-specific ports and volumes.
    - Implement entrypoint script for Docker to manage application start-up and hot reloading.
    - Update application properties and Maven configuration to align with new structure and dependencies.

---
+++ backend/src/main/java/com/harmonical/backend/HarmonicalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harmonical.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonicalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmonicalBackendApplication.class, args);
+    }
+
+}
