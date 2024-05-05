cf419b8285c9166726894478bb69e6b64db0f042
Manne Emile KITSOUKOU <emilemannekitsoukou@gmail.com>
2024-05-05 16:08:37 +0200

    chore(repository): enable JpaSpecificationExecutor in JPAEventRepository

--- backend/src/main/java/com/harmonical/backend/infrastructure/persistence/repository/JPAEventRepository.java
+++ backend/src/main/java/com/harmonical/backend/infrastructure/persistence/repository/JPAEventRepository.java
@@ -2,10 +2,11 @@ package com.harmonical.backend.infrastructure.persistence.repository;
 
 import com.harmonical.backend.infrastructure.persistence.model.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface JPAEventRepository extends JpaRepository<EventEntity, UUID> {
+public interface JPAEventRepository extends JpaRepository<EventEntity, UUID>, JpaSpecificationExecutor<EventEntity> {
 }
